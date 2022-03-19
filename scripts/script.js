21ff6af8d0b09dd90f87cb7320f1f10505a93610
Maria P <misterrian@yandex.ru>
2022-03-19 22:53:00 +0300

    finalizing scripts

---
+++ scripts/script.js
@@ -0,0 +1,35 @@
+const formElement = document.querySelector('.popup');
+//formElement.addEventListener('submit', formSubmitHandler);
+
+const saveButton = document.querySelector('.popup__submit-button');
+saveButton.addEventListener('click', formSubmitHandler);
+
+const penButton = document.querySelector('.profile-info__edit-button');
+penButton.addEventListener('click', showPopup);
+
+const profileTitle = document.querySelector('.profile-info__title');
+const profileSubtitle = document.querySelector('.profile-info__subtitle');
+const nameInput = formElement.querySelector('#name');
+const jobInput = formElement.querySelector('#job');
+
+const closeButton = formElement.querySelector('.popup__close-icon');
+closeButton.addEventListener('click', hidePopup);
+
+function showPopup() {
+    nameInput.value = profileTitle.textContent;
+    jobInput.value = profileSubtitle.textContent;
+    formElement.classList.add('popup_opened');
+}
+
+function hidePopup() {
+    formElement.classList.remove('popup_opened');
+}
+
+function formSubmitHandler(evt) {
+    evt.preventDefault();
+
+    profileTitle.textContent = nameInput.value;
+    profileSubtitle.textContent = jobInput.value;
+
+    hidePopup();
+}
