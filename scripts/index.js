63b4518d3079095f5f5d4348601918af029acc70
Maria P <misterrian@yandex.ru>
2022-03-25 09:29:58 +0300

    review fix

--- scripts/index.js
+++ scripts/index.js
@@ -1,25 +1,26 @@
-const formElement = document.querySelector('.popup');
-formElement.addEventListener('submit', formSubmitHandler);
+const popupElement = document.querySelector('.popup');
+const popupContainer = popupElement.querySelector('.popup__container');
+popupContainer.addEventListener('submit', formSubmitHandler);
 
 const penButton = document.querySelector('.profile-info__edit-button');
 penButton.addEventListener('click', showPopup);
 
 const profileTitle = document.querySelector('.profile-info__title');
 const profileSubtitle = document.querySelector('.profile-info__subtitle');
-const nameInput = formElement.querySelector('#name');
-const jobInput = formElement.querySelector('#job');
+const nameInput = popupElement.querySelector('#name');
+const jobInput = popupElement.querySelector('#job');
 
-const closeButton = formElement.querySelector('.popup__close-icon');
+const closeButton = popupElement.querySelector('.popup__close-icon');
 closeButton.addEventListener('click', hidePopup);
 
 function showPopup() {
     nameInput.value = profileTitle.textContent;
     jobInput.value = profileSubtitle.textContent;
-    formElement.classList.add('popup_opened');
+    popupElement.classList.add('popup_opened');
 }
 
 function hidePopup() {
-    formElement.classList.remove('popup_opened');
+    popupElement.classList.remove('popup_opened');
 }
 
 function formSubmitHandler(evt) {
